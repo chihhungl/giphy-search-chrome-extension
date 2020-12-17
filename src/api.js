11e81f84843e5bc5742731324a26f9ec409f473d
Chihhung <chihhung.lin@mycase.com>
2020-12-16 22:18:01 -0800

    Remove api key

--- src/api.js
+++ src/api.js
@@ -1,5 +1,8 @@
+import { API_KEY } from './secret';
+
+const BASE_URL = 'https://api.giphy.com/v1/gifs';
+
 export function searchGiphy(searchTerm) {
-  const apiKey = 'vTIpYBc3I5jgBnqWKy1SMccAAXfpSRvy';
-  return fetch(`https://api.giphy.com/v1/gifs/search?&api_key=${apiKey}&q=${searchTerm}&limit=50`)
+  return fetch(`${BASE_URL}/search?&api_key=${API_KEY}&q=${searchTerm}&limit=50`)
       .then(resp => resp.json());
 }
